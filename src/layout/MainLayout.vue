536fb0cbc1088a31cd2493e25937d9a5f81f850a
Cirtl <70183047+Cirtl@users.noreply.github.com>
2021-08-21 10:13:44 +0800

    feat: 顶部栏和默认路径

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,18 +1,28 @@
 <template>
     <el-container style="solid #eee">
-        <el-aside width="125px">
-            <side-bar/>
-        </el-aside>
+        <el-header>
+            <top-bar></top-bar>
+        </el-header>
         <el-container>
-            <router-view/>
+            <el-aside width="125px">
+                <side-bar/>
+            </el-aside>
+            <el-container>
+                <router-view/>
+            </el-container>
         </el-container>
     </el-container>
 </template>
 
 <script>
 import SideBar from '../components/SideBar.vue'
+import TopBar from '../components/TopBar.vue'
 export default {
-  components: { SideBar },
-    
+    components: { SideBar, TopBar },
+    created() {
+        if (this.$route.path === '/') {
+            this.$router.replace('/list')
+        }
+    }
 }
 </script>
