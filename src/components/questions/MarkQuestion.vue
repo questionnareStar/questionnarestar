b7dffec8decc31b57a6f6eb5dcbc061db733ae14
Cirtl <70183047+Cirtl@users.noreply.github.com>
2021-08-23 16:26:39 +0800

    feat: 问卷页面题目展示

---
+++ src/components/questions/MarkQuestion.vue
@@ -0,0 +1,54 @@
+<template>
+    <div style="width=85% mb-8 height=100px">
+        <el-row class="question-title mb-8">
+            <span v-if="serial">{{sequence}}</span>
+            <span>{{question}}</span>
+            <span v-if="required" class="red">*</span>
+        </el-row>
+        <el-row v-if="description" class="question-des mb-8">
+            <span>{{description}}</span>
+        </el-row>
+        <el-row class="question-choices mb-8">
+            <el-rate v-model="answers[0]" :maxScore="maxScore" @change="onchange()"/>
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['id', 'serial', 'sequence', 'question', 'required', 'description', 'maxScore'],
+    data() {
+        return {
+            answers: []
+        }
+    },
+    methods: {
+        onchange() {
+            let data = {
+                answerList: this.answers,
+                itemType: 2,
+                topicId: this.$props.id
+            }
+            this.$emit('getAnswer', data)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.mb-8 {
+    margin-bottom: 8px;
+}
+.question-title > span {
+    font-size: 18px;
+    font-weight: bold;
+    margin-right: 10px;
+}
+.question-des > span {
+    font-size: 13px;
+    font-weight:lighter;
+}
+.red {
+    color: red;
+}
+</style>
---
+++ src/components/questions/SingleSelect.vue
@@ -0,0 +1,61 @@
+<template>
+    <div style="width=85% mb-8">
+        <el-row class="question-title mb-8">
+            <span v-if="serial">{{sequence}}</span>
+            <span>{{question}}</span>
+            <span v-if="required" class="red">*</span>
+        </el-row>
+        <el-row v-if="description" class="question-des mb-8">
+            <span>{{description}}</span>
+        </el-row>
+        <el-row class="question-choices mb-8">
+            <el-radio-group v-model="answers[0]" @change="onchange()">
+                <el-radio class="choice-block" v-for="(item, index) in choices" :key="index" :label="item">{{item}}</el-radio>
+            </el-radio-group>
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['id', 'serial', 'sequence', 'question', 'required', 'description', 'choices'],
+    data() {
+        return {
+            answers: []
+        }
+    },
+    methods: {
+        onchange() {
+            let data = {
+                answerList: this.answers,
+                itemType: 4,
+                topicId: this.$props.id
+            }
+            this.$emit('getAnswer', data)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.choice-block {
+    display: block;
+    margin: 3px;
+    font-size: 16px;
+}
+.mb-8 {
+    margin-bottom: 8px;
+}
+.question-title > span {
+    font-size: 18px;
+    font-weight: bold;
+    margin-right: 10px;
+}
+.question-des > span {
+    font-size: 13px;
+    font-weight:lighter;
+}
+.red {
+    color: red;
+}
+</style>
