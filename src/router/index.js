39b23416f9e27f34409b72c9236b612c649de25e
13321187597 <goutrui@163.com>
2021-08-28 14:56:20 +0800

    fix:中转页和主界面修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import {message} from '../util/inform'
+import {
+  message
+} from '../util/inform'
 // 布局文件
 import MainLayout from '../layout/MainLayout.vue'
 import AuthLayout from '../layout/AuthLayout.vue'
@@ -18,115 +20,149 @@ import Register from '../views/Register.vue'
 import Questionnare from '../views/Questionnare.vue'
 import Preview from '../views/Preview.vue'
 import Finish from '../views/Finish.vue'
+import TransitionPage from '../views/TransitionPage.vue'
 
 Vue.use(VueRouter)
 
-const MainChildRoutes = [
-  {
+const MainChildRoutes = [{
     path: 'list',
     name: 'list',
     component: QuestionnareList,
-    meta: { sign: true },
+    meta: {
+      sign: true
+    },
   },
   {
     path: 'waste',
     name: 'waste',
     component: Waste,
-    meta: { sign: true },
+    meta: {
+      sign: true
+    },
   },
   {
     path: 'edit',
     name: 'edit',
     component: Edit,
-    meta: { sign: true },
+    meta: {
+      sign: true
+    },
   },
   {
     path: 'publish',
     name: 'publish',
     component: Publish,
-    meta: { sign: true }
+    meta: {
+      sign: true
+    }
   },
   {
     path: 'welcome',
     name: 'welcome',
     component: Welcome,
-    meta: { sign: false }
+    meta: {
+      sign: false
+    }
   },
   {
     path: 'create',
     name: 'create',
     component: CreateQuestionnare,
-    meta: { sign: true }
+    meta: {
+      sign: true
+    }
+  },
+  {
+    path: '/trans',
+    name: 'Trans',
+    component: TransitionPage,
+    meta: {
+      sign: false
+    }
   }
 ]
 
-const AuthChildRoutes = [
-  {
+const AuthChildRoutes = [{
     path: 'login',
     name: 'login',
     component: Login,
-    meta: { sign: false },
+    meta: {
+      sign: false
+    },
   },
   {
     path: 'register',
     name: 'register',
     component: Register,
-    meta: { sign: false },
+    meta: {
+      sign: false
+    },
   },
   {
     path: 'validate/:id',
     name: 'validate',
     component: Login,
-    meta: { sign: false }
+    meta: {
+      sign: false
+    }
   }
 ]
 
-const QuestionnareChildRoutes = [
-  {
-    path: ''
-  }
-]
+const QuestionnareChildRoutes = [{
+  path: ''
+}]
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'MainLayout',
     component: MainLayout,
     children: MainChildRoutes,
-    meta: { sign: false }
+    meta: {
+      sign: false
+    }
   },
   {
     path: '/auth',
     name: 'AuthLayout',
     component: AuthLayout,
     children: AuthChildRoutes,
-    meta: { sign: false }
+    meta: {
+      sign: false
+    }
   },
-    {
+  {
     path: '/statistics',
     name: 'statistics',
     component: () => import("../views/statistics.vue"),
-    meta: { sign: false }
+    meta: {
+      sign: false
+    }
   },
   {
     path: '/questionnare/:id',
     name: 'Questionnare',
     component: Questionnare,
-    meta: { sign: false }
+    meta: {
+      sign: false
+    }
   },
   {
     // 0 为回收站， 1 为列表
     path: '/preview/:from/:id',
     name: 'Preview',
     component: Preview,
-    meta: { sign: true }
+    meta: {
+      sign: true
+    }
   },
   {
     path: '/finish',
     name: 'Finish',
     component: Finish,
-    meta: { sign: false }
-  }
+    meta: {
+      sign: false
+    }
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/TransitionPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="demo-image">
+    <div class="block">
+      <el-image
+        style="width: 400px; height: 300px"
+        :src="imgUrl1"
+      ></el-image>
+    </div>
+  </div>
+
+</template>
+<script>
+import jpg1 from '../assets/问卷1.png'
+import jpg2 from '../assets/问卷2.png'
+import jpg3 from '../assets/问卷3.png'
+import jpg4 from '../assets/问卷4.png'
+import jpg5 from '../assets/Questionlogo.png'
+
+export default {
+  data:function(){
+    return {
+      imgUrl1: jpg1
+    }
+  }
+}
+</script>
