afef06f8e65abf9903892a990d3ea8240577cdfc
NaCN腌鱼 <71486154+19373282@users.noreply.github.com>
2021-08-28 14:14:19 +0800

    Update list.js

--- src/util/service/list.js
+++ src/util/service/list.js
@@ -1,3 +1,11 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: 张怡健
+ * @Date: 2021-08-22 15:40:09
+ * @LastEditors: 张怡健
+ * @LastEditTime: 2021-08-28 10:24:07
+ */
 import axios from './index'
 
 export default {
@@ -42,22 +50,22 @@ export default {
             data: data
         })
     },
-    recovery(data) {
+    recovery(data){
         return axios({
             headers: {
                 "token": JSON.parse(localStorage.getItem('userInfo')).token
             },
             method: 'PUT',
-            url: '/api/v1/questionnaire/restore/' + data,
+            url: '/api/v1/questionnaire/restore/'+data,
         })
     },
-    deleteDeath(data) {
+    deleteDeath(data){
         return axios({
             headers: {
                 "token": JSON.parse(localStorage.getItem('userInfo')).token
             },
             method: 'DELETE',
-            url: '/api/v1/questionnaire/death/' + data,
+            url: '/api/v1/questionnaire/death/'+data,
             timeout: 5000
         })
     },
@@ -100,9 +108,29 @@ export default {
                 "token": JSON.parse(localStorage.getItem('userInfo')).token
             },
             method: 'POST',
-            url: '/api/v1/statics/get' + "?questionnaireId=" + data,
+            url: '/api/v1/statics/get'+"?questionnaireId="+data,
             questionnaireId: data
         })
     },
+    getStatisticsWhole(data) {
+        return axios({
+            headers: {
+                "token": JSON.parse(localStorage.getItem('userInfo')).token
+            },
+            method: 'POST',
+            url: '/api/v1/statics/answer/number'+"?questionnaireId="+"11",
+            questionnaireId: data
+        })
+    },
+    getStatisticsDetail(data1,data2){
+        return axios({
+            headers: {
+                "token": JSON.parse(localStorage.getItem('userInfo')).token
+            },
+            method: 'POST',
+            url: '/api/v1/statics/answer/detail',
+            questionnaireId:data1,
+            recordId:data2
+        })
+    }
 }
-
