6d8be239955f68e58c50192a5f3e3bfc82dca7eb
NaCN腌鱼 <71486154+19373282@users.noreply.github.com>
2021-08-29 20:37:18 +0800

    fix：增添返回首页

--- src/views/Finish.vue
+++ src/views/Finish.vue
@@ -1,7 +1,22 @@
+
 <template>
+<div>
     <div class="bg"></div>
+         <div class="back-container">
+           <el-button type="primary" round @click="back()">点我返回首页</el-button>
+        </div></div>
 </template>
 
+<script>
+
+export default {
+    methods: {
+        back(){
+            this.$router.push("/welcome")
+        }
+    }
+}
+</script>
 <style scoped>
 .bg {
     background-image: url('../assets/finish.jpg');
@@ -10,5 +25,8 @@
     background-position: center;
     width: 100%;
     height: 500px;
+}
+.back-container{
+   text-align:center;
 }
 </style>
