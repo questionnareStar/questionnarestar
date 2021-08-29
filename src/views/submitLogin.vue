7daadf574254276c571261ca7f4a69afc150a304
NaCN腌鱼 <71486154+19373282@users.noreply.github.com>
2021-08-29 20:38:51 +0800

    Add files via upload

---
+++ src/views/submitLogin.vue
@@ -0,0 +1,172 @@
+<template>
+    <div>
+        <h2>用户登录</h2>
+        <el-form :model="loginInfo">
+            <el-form-item label="邮箱">
+                <el-input placeholder="请输入邮箱" v-model="loginInfo.email"/>
+            </el-form-item>
+            <el-form-item v-if="isPassMode" label="密码">
+                <el-input type="password" placeholder="请输入密码" v-model="loginInfo.password"/>
+            </el-form-item>
+            <el-form-item v-else label="验证码">
+                <el-input placeholder="请输入验证码" v-model="loginInfo.code"/>
+            </el-form-item>
+        </el-form>
+        <el-button v-if="!isPassMode" @click="fetchCode()">获取验证码</el-button>
+        <el-button type="primary" @click="login()">登录</el-button>
+        <el-divider/>
+        <div class="other-choice">
+            <span @click="changeLoginMode()">验证码登录</span>
+            <el-divider direction="vertical"/>
+            <span @click="toRegister()">注册账号</span>
+        </div>
+    </div>
+</template>
+
+<script>
+import auth from "../util/service/auth.js"
+import { message } from "../util/inform.js"
+
+export default {
+    data() {
+        return {
+            isPassMode: true,
+            loginInfo: {
+                email: "",
+                password: "",
+                code: ""
+            }
+        }
+    },
+    methods: {
+        login() {
+            if (this.isPassMode) {
+                if (this.loginInfo.email !== '' && this.loginInfo.password !== '') {
+                    let formData = {
+                        account: this.loginInfo.email,
+                        password: this.loginInfo.password
+                    }
+                    auth.passwordLogin(formData)
+                        .then((response) => {
+                            const data = response.data
+                            console.log(response)
+                            if (data.code === 20000) {
+                                message({
+                                    message: '登录成功',
+                                    type: 'success'
+                                })
+                                localStorage.setItem('userInfo', JSON.stringify(data.data))
+                                this.afterLogin()
+                            } else {
+                                message({
+                                    message: data.msg,
+                                    type: 'error'
+                                })
+                            }
+                        })
+                        .catch((error) => {
+                            message({
+                                message: error.message,
+                                type: 'warning'
+                            })
+                        })
+                } else {
+                    message({
+                        message: '请正确输入邮箱和密码',
+                        type: 'error'
+                    })
+                }
+            } else {
+                if (this.loginInfo.email !== '' && this.loginInfo.code !== '') {
+                    let formData = {
+                        account: this.loginInfo.email,
+                        code: this.loginInfo.code
+                    }
+                    auth.codeLogin(formData)
+                        .then((response) => {
+                            const data = response.data
+                            console.log(response)
+                            if (data.code === 20000) {
+                                message({
+                                    message: '登录成功',
+                                    type: 'success'
+                                })
+                                localStorage.setItem('userInfo', JSON.stringify(data.data))
+                                this.afterLogin()
+                            } else {
+                                message({
+                                    message: data.msg,
+                                    type: 'error'
+                                })
+                            }
+                        })
+                        .catch((error) => {
+                            message({
+                                message: error.message,
+                                type: 'warning'
+                            })
+                        })
+                } else {
+                    message({
+                        message: '请正确输入邮箱和验证码',
+                        type: 'error'
+                    })
+                }
+            }
+        },
+        changeLoginMode() {
+            this.isPassMode = !this.isPassMode
+        },
+        fetchCode() {
+            if (/^(\w+)(\.\w+)*@(\w+)(\.\w+)*.(\w+)$/i.test(this.loginInfo.email)) {
+                this.gotCode = true
+                auth.fetchCodeForLogin(this.loginInfo.email)
+                    .then((response) => {
+                        console.log(response)
+                        const data = response.data
+                        if (data.code === 20000) {
+                            message({
+                                message: '验证码获取成功',
+                                type: 'success'
+                            })
+                        } else {
+                            message({
+                                message: data.msg,
+                                type: 'error'
+                            })
+                        }
+                    })
+                    .catch((error) => {
+                        message({
+                            message: error.message,
+                            type: 'warning'
+                        })
+                    })
+            } else {
+                message({
+                message: '正确输入邮箱后方可获取验证码',
+                type: 'error'
+                })
+            }
+        },
+        toRegister() {
+            this.$router.push('/auth/submitregister')
+        },
+        afterLogin() {
+         this.$router.push(JSON.parse(localStorage.getItem('Suburl')))
+        }
+    }
+}
+</script>
+
+<style scoped>
+.other-choice {
+    display: inline-flex;
+    margin-bottom: 25px;
+}
+.other-choice span {
+    color:#2f71dc;
+    margin-inline: 15px;
+    cursor: pointer;
+}
+</style>
---
+++ src/views/submitRegister.vue
@@ -0,0 +1,177 @@
+<template>
+  <div>
+        <h2>注册用户</h2>
+        <el-form
+          :model="registerInfo"
+          status-icon
+          label-position="top"
+          :rules="rules"
+          ref="registerInfo">
+          <el-form-item label="邮箱" prop="email">
+            <el-input placeholder="请输入邮箱" v-model="registerInfo.email"/>
+          </el-form-item>
+          <el-form-item label="密码" prop="pass">
+            <el-input type="password" placeholder="请输入密码" v-model="registerInfo.pass"/>
+          </el-form-item>
+          <el-form-item label="确认密码" prop="checkPass">
+            <el-input type="password" placeholder="请再次输入密码" v-model="registerInfo.checkPass"/>
+          </el-form-item>
+          <el-form-item label="验证码" prop="code">
+            <el-input placeholder="请输入验证码" v-model="registerInfo.code" :disabled="!gotCode"/>
+          </el-form-item>
+        </el-form>
+        <el-button @click="fetchCode()">获取验证码</el-button>
+        <el-button type="primary" @click="register('registerInfo')">注册</el-button>
+        <el-divider/>
+        <div class="other-choice">
+            <span @click="toLogin()">已有帐号？返回登录</span>
+        </div>
+    </div>
+</template>
+
+<script>
+import { message } from '../util/inform'
+import auth from '../util/service/auth'
+export default {
+  data() {
+    var checkEmail = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("邮箱不能为空"));
+      } else if (/^(\w+)(\.\w+)*@(\w+)(\.\w+)*.(\w+)$/i.test(value)){
+        callback();
+      } else {
+        return callback(new Error("邮箱格式不正确"));
+      }
+    }
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (value.length < 6 || value.length > 20) {
+          callback(new Error("请输入六至二十位"));
+        }
+        // var regx = /^(?!([a-zA-Z]+|\d+)$)[a-zA-Z\d]{6,20}$/;
+        // if (!this.registerInfo.pass.match(regx)) {
+        //   callback(new Error("请同时包含字母数字"));
+        // }
+        if (this.registerInfo.checkPass !== "") {
+          this.$refs.registerInfo.validateField("checkPass");
+        }
+        callback();
+      }
+    }
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.registerInfo.pass) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    }
+    var validateCode = (rule, value, callback) => {
+      if (value) {
+        callback()
+      } else {
+        callback(new Error("验证码不能为空"))
+      }
+    }
+    return {
+      registerInfo: {
+        email: "",
+        pass: "",
+        checkPass: "",
+        code: ""
+      },
+      rules: {
+        email: [{ validator: checkEmail, trigger: "blur" }],
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }],
+        code: [{ validator: validateCode, trigger: "blur" }]
+      },
+      realCode: "",
+      gotCode: false
+    };
+  },
+  methods: {
+    register(registerInfo) {
+      this.$refs[registerInfo].validate((valid) => {
+          if (valid) {
+            let formData = {
+              account: this.registerInfo.email,
+              code: this.registerInfo.code,
+              password: this.registerInfo.pass
+            }
+            auth.register(formData)
+              .then((response) => {
+                console.log(response)
+                const data = response.data
+                if (data.code === 20000) {
+                  message({
+                    message: data.msg,
+                    type: 'success'
+                  })
+                  this.$router.push('/auth/subumitlogin')
+                } else {
+                  message({
+                    message: data.msg,
+                    type: 'error'
+                  })
+                }
+              })
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+    },
+    toLogin() {
+      this.$router.push('/auth/submitlogin');
+    },
+    fetchCode() {
+      if (/^(\w+)(\.\w+)*@(\w+)(\.\w+)*.(\w+)$/i.test(this.registerInfo.email)) {
+        this.gotCode = true
+        auth.fetchCodeForRegister(this.registerInfo.email)
+          .then((response) => {
+            console.log(response)
+            const data = response.data
+            if (data.code === 20000) {
+              message({
+                message: '验证码获取成功',
+                type: 'success'
+              })
+            } else {
+              message({
+                message: data.msg,
+                type: 'error'
+              })
+            }
+          })
+          .catch((error) => {
+              message({
+                  message: error.message,
+                  type: 'warning'
+              })
+          })
+      } else {
+        message({
+          message: '正确输入邮箱后方可获取验证码',
+          type: 'error'
+        })
+      }
+    }
+  },
+};
+</script>
+
+<style scoped>
+.other-choice {
+    display: inline-flex;
+    margin-bottom: 25px;
+}
+.other-choice span {
+    color:#2f71dc;
+    margin-inline: 15px;
+    cursor: pointer;
+}
+</style>
